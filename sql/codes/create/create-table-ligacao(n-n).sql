06c93bcbfbad029b3bed316f9518acbfcce5b10f
Enzo Ribas <enzortorres2005@gmail.com>
2024-10-17 20:17:57 -0300

    studying

--- sql/codes/create/create-table-ligacao(n-n).sql
+++ sql/codes/create/create-table-ligacao(n-n).sql
@@ -1,8 +1,8 @@
 CREATE TABLE ocorrencia (
     codmulta INTEGER,
     placa CHAR(7),
-    datamulta DATE,
     codagente INTEGER,
+    datamulta DATE,
     PRIMARY KEY(codmulta, placa, datamulta), -- SÓ DEFINE A CHAVE PRIMÁRIA EMBAIXO QUANDO É UMA CHAVE PRIMÁRIA COMPOSTA
     FOREIGN KEY(codmulta) REFERENCES multas(codmulta),
     FOREIGN KEY(placa) REFERENCES carro(placa),
